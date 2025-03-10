e4e0d1627d60563568162a68a02044582f74a40e
firasboudhraa <fboudhraa2@gmail.com>
2025-03-10 16:27:22 +0100

    initial Setup

---
+++ src/test/java/tn/esprit/backend_pi/BackendPiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.backend_pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
